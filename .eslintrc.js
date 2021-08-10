50b6e1d0f276444d622766ef25d5d9f477c6fdb9
zedang <ze.dang@thoughtworks.com>
2021-08-10 11:10:38 +0800

    chore: add import order rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: ["react-app", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "simple-import-sort"],
   rules: {
     "prettier/prettier": "error",
     "arrow-body-style": "off",
     "prefer-arrow-callback": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
   },
 };
